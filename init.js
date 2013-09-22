34ca109223c2b07f63f4f24f6a56ce8462cd65a8
daeks <daeks@gmx.net>
2013-09-22 23:15:40 +0200

    fixed separator

--- init.js
+++ init.js
@@ -31,7 +31,13 @@
                 var response = jQuery.parseJSON(data);
                 jQuery.each(response, function(i, val) {
                   if(val['t'] == 'context-menu') {
-                    var macro = '<hr class="'+val['a']+'"><a class="'+val['a']+'" onclick="codiad.macro.execute(\''+i+'\','+val['d']+', $(\'#context-menu\').attr(\'data-path\'));"><span class="icon-'+val['i']+'"></span>'+val['n']+'</a>';
+                    $('#'+val['t']).append('<hr class="'+val['a']+'">');
+                    return false;
+                  }
+                });
+                jQuery.each(response, function(i, val) {
+                  if(val['t'] == 'context-menu') {
+                    var macro = '<a class="'+val['a']+'" onclick="codiad.macro.execute(\''+i+'\','+val['d']+', $(\'#context-menu\').attr(\'data-path\'));"><span class="icon-'+val['i']+'"></span>'+val['n']+'</a>';
                     $('#'+val['t']).append(macro);
                   }
                   if(val['t'] == 'sb-right-content') {
